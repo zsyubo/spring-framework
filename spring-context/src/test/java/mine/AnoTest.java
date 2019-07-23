c308056c73cf0cd74a1eae6b2de187dd9d62d135
huyuanfu <hu678>
2019-07-23 16:51:32 +0800

    测试代码提交

--- spring-context/src/test/java/mine/AnoTest.java
+++ spring-context/src/test/java/mine/AnoTest.java
@@ -9,6 +9,7 @@ public class AnoTest {
 		org.springframework.context.annotation.AnnotationConfigApplicationContext annotationConfigApplicationContext
 				= new org.springframework.context.annotation.AnnotationConfigApplicationContext( MySpringConfig.class );
 		System.out.println("懒汉，恶汉？");
+
 		UserEntity userEntity = annotationConfigApplicationContext.getBean("userEntity", UserEntity.class);
 		System.out.println("UserEntity: "+userEntity.toString());
 	}
